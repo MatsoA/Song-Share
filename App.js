580719ce365afe694281e59ea47c857a33848d89
MatsoA <19639672+MatsoA@users.noreply.github.com>
2023-02-01 23:13:24 -0500

    Set up sign in with google from popup
    added firebase to project and set up sign in with popup

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { firebaseApp, authProvider } from "./firebaseConfig";
+import Signin from "./Signin"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 
 export default function App() {
+
+  //calling Signin component as a function (since it's exported as one). Triggers signin popup
+  Signin();
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
---
+++ Signin.js
@@ -0,0 +1,32 @@
+// Initialize Firebase
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { firebaseApp, authProvider } from "./firebaseConfig";
+
+
+export default function Signin() {
+
+    const auth = getAuth();
+
+    signInWithPopup(auth, authProvider)
+    .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        
+        userInfo = user;
+
+        // ...
+    }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+    });
+}
+
